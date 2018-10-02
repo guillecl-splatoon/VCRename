71e6a9d9eb2e8f3ce60c7936134c4d0d20acb8ec
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-02 15:45:06 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ BOT_PREFIX = '='
 TOKEN = "NDk2NTEwOTMyNDc5MTE1Mjg0.DpRsZQ.xPZPqVxLft6tdhv0LnxNmX004ok"
 SERVER = '496104544040910858'
 LOG_CHANNEL = '496112192488210432'
-VOICE_CHANNELS = ['496118243929620488','496118274908618752']
+VOICE_CHANNELS = ['496118243929620488' : '1','496118274908618752' : '2']
 DEFAULT_NAME = "Otros Juegos "
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -21,13 +21,11 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(before, after):
 	if before.voice.voice_channel is not after.voice.voice_channel:
-		count = 0
 		for channel in client.get_all_channels():
 			if str(channel.type) == "voice":
 				if channel.id in VOICE_CHANNELS:
-					count = count + 1
 					if not channel.voice_members:
-						await client.edit_channel(channel,name=DEFAULT_NAME + str(count))
+						await client.edit_channel(channel,name=DEFAULT_NAME + VOICE_CHANNELS[channel.id])
 
 @client.command()
 async def ping():
