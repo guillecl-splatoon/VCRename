a6a5017d747446f1b1f3db63b3a36b34022b0f51
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-02 19:37:02 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ async def say(ctx, arg):
 @client.command(pass_context=True)
 async def rename(ctx, arg):
 	await client.send_message(client.get_channel('496104544040910860'), ctx.message.author.voice.voice_channel.id)
-	if ctx.message.author.voice.voice_channel.id in VOICE_CHANNELS:
+	if ctx.message.author.voice.voice_channel.id in VOICE_CHANNELS.keys():
 		await client.send_message(client.get_channel(LOG_CHANNEL), ctx.message.author.display_name + " ha cambiado el nombre de la sala " + ctx.message.author.voice.voice_channel.name + " a " + arg)
 		await client.edit_channel(ctx.message.author.voice.voice_channel,name=arg)
 
