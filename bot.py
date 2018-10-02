b6c69ed3b4aef25e732d64244dfe5e0c18f3fb97
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-02 10:15:06 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
 import asyncio
+from discord.ext.commands import Bot
 
-BOT_PREFIX = "="
+BOT_PREFIX = '='
 TOKEN = "NDk2NTEwOTMyNDc5MTE1Mjg0.DpRsZQ.xPZPqVxLft6tdhv0LnxNmX004ok"
 SERVER = '496104544040910858'
 LOG_CHANNEL = '496112192488210432'
@@ -9,6 +10,7 @@ VOICE_CHANNELS = ['496118243929620488','496118274908618752']
 DEFAULT_NAME = "Otros Juegos"
 
 client = discord.Client()
+bot = Bot(command_prefix=BOT_PREFIX)
 
 @client.event
 async def on_ready():
@@ -16,6 +18,26 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+##@client.event
+##async def on_message(message):
+##	if message.content.startswith('=ping'):
+##        	await client.send_message(message.channel, 'pong')
+##
+##	elif message.content.startswith('=rename'):
+##		nombre = message.content.split(' ')[1]
+##		await client.send_message(message.channel, nombre)
+#		if message.author.voice.voice_channel in VOICE_CHANNELS:
+#			await client.send_message(client.get_channel(LOG_CHANNEL), message.author.display_name + "has changed the name of " + client.get_channel(message.author.voice.voice_channel).name + "to " + nombre
+#			client.get.channel(message.author.voice.voice_channel).name = nombre
+
+@bot.command()
+async def ping(ctx):
+	await client.send_message(ctx.message.channel, "Pong! " + ctx.author.display_name)
+	
+client.run(TOKEN)
+
+############# BACKUP #############
 
 #def cmd(name, perms, description, *aliases):
 #    def real_decorator(func):
@@ -27,19 +49,7 @@ async def on_ready():
 #                print("ERROR: Cannot assign alias {0} to command {1} since it is already the name of a command!".format(alias, name))
 #        return func
 #    return real_decorator
-	
-@client.event
-async def on_message(message):
-	if message.content.startswith('=ping'):
-        	await client.send_message(message.channel, 'pong')
-	
-	elif message.content.startswith('=rename'):
-		nombre = message.content.split(' ')[1]
-		await client.send_message(message.channel, nombre)
-#		if message.author.voice.voice_channel in VOICE_CHANNELS:
-#			await client.send_message(client.get_channel(LOG_CHANNEL), message.author.display_name + "has changed the name of " + client.get_channel(message.author.voice.voice_channel).name + "to " + nombre
-#			client.get.channel(message.author.voice.voice_channel).name = nombre
-############# COMMANDS #############
+
 #@cmd('nombre', [0,0], "```\n{0}Renames a channel.```")
 #async def cmd_rename(message, parameters):
 #    if parameters == '':
@@ -48,7 +58,7 @@ async def on_message(message):
 #		if message.author.voice.voice_channel in VOICE_CHANNELS:
 #			await client.send_message(client.get_channel(LOG_CHANNEL), message.author.display_name + "has changed the name of " + client.get_channel(message.author.voice.voice_channel).name + "to " + parameters
 #			client.get.channel(message.author.voice.voice_channel).name = parameters			
-	
+
 #@cmd('ping', [0,0], "```\n{0}Ping.```")
 #async def cmd_ping(message, parameters):
 #	await reply(message, "ping")					  
@@ -58,4 +68,3 @@ async def on_message(message):
 #	if before.voice.voice_channel is not after.voice.voice_channel:
 #        	if len(before.voice.voice_channel.voice_members) == 0:
 #			before.voice.voice_channel.name = DEFAULT_NAME
-client.run(TOKEN)
