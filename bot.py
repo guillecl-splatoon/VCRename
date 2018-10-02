8e18c74210ccb72b804edd977c6001176f225d6d
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-02 11:14:45 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def ping():
 
 @client.command(pass_context=True)
 async def test(ctx):
-    await client.send_message(client.get_channel('496104544040910860'), ctx.message.author.display_name)
+    await client.send_message(ctx.message.channel.id, ctx.message.author.display_name)
 
 client.run(TOKEN)
 
