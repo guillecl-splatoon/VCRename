95ed2c8438ddceb22e7b67a7aaa5ea7b8212a532
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-02 01:38:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def on_message(message):
 	elif message.content.startswith('=rename'):
 		nombre = message.content.split(' ')[1]
 		if message.author.voice.voice_channel in VOICE_CHANNELS:
-			await client.send_message(client.get_channel(LOG_CHANNEL), message.author.display_name + "has changed the name of " + client.get_channel(message.author.voice.voice_channel).name + "to " + parameters
+			await client.send_message(client.get_channel(LOG_CHANNEL), message.author.display_name + "has changed the name of " + client.get_channel(message.author.voice.voice_channel).name + "to " + nombre
 			client.get.channel(message.author.voice.voice_channel).name = parameters
 ############# COMMANDS #############
 #@cmd('nombre', [0,0], "```\n{0}Renames a channel.```")
