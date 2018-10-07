f8addc3471b8b3851d5ae606d231d05b4b050618
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-06 23:26:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import discord
 import asyncio
-import config
 from discord.ext.commands import Bot
+
+BOT_PREFIX = '='
+TOKEN = "NDk4MzE4MTc2MDE5NTQ2MTIz.Dpr-pw._rNgc4sCzfovhoHO1caK5gWPSTY"
+LOG_CHANNEL = "351293761642561557"
+VOICE_CHANNELS = {"496118243929620488" : 1,"496118274908618752" : 2}
+DEFAULT_NAME = "Otros Juegos "
 
 client = Bot(command_prefix=BOT_PREFIX)
 
