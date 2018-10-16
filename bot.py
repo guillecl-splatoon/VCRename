a91757b2f3438c78cbd1aea4ab5989525c2ec290
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-10-16 19:12:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,9 @@ async def say(ctx, arg):
 @client.command(pass_context=True)
 async def rename(ctx, arg):
 	if ctx.message.author.voice.voice_channel.id in VOICE_CHANNELS:
-		await client.send_message(client.get_channel(LOG_CHANNEL), ctx.message.author.mention + " ha cambiado el nombre de la sala " + str(ctx.message.author.voice.voice_channel) + " a " + arg)
+		embed = discord.Embed(title="Tile", description="Desc", color=0xfce016)
+        	embed.add_field(name="Field1", value="ctx.message.author.mention + " ha cambiado el nombre de la sala " + str(ctx.message.author.voice.voice_channel) + " a " + arg", inline=False)
+		await client.send_message(client.get_channel(LOG_CHANNEL), embed=embed)
 		await client.edit_channel(ctx.message.author.voice.voice_channel,name=arg)
 		
 client.run(TOKEN)
